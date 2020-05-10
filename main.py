ac4d05d21978ff2f0fbbe9fac44283e71c8e9420
alexander <alexander.kotrin@mail.ru>
2020-05-10 11:08:32 +0300

    checking 2

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-print('This is dev branch!')
-
 print("Hello Takewill!")
-
-print('dev')
-print("this is new test")
-print('nu pot')
-print('This is master branch!')
-
-print('hi takewill!')
-
-print("Hi Man!")
-
+print('2')
+print('3')
 print('4')
-
 print('5')
-
+print('6')
